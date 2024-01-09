0baf01fc76f82ddbfa3a06c92fcc166311c31009
Pranov Sarath <pranov03@gmail.com>
2024-01-09 00:32:35 +0000

    Completed Day 4 OOP

--- Resources/04_OOPUnderTheHood/learn/app.js
+++ Resources/04_OOPUnderTheHood/learn/app.js
@@ -7,34 +7,47 @@
 // }
 
 
-// class Dog{
-//     constructor(name, breed){
-//         this.name = name;
-//         this.breed = breed;
-//     }
+class Dog{
+    constructor(name, breed){
+        this.name = name;
+        this.breed = breed;
+    }
 
-//     bark(){
-//         return `${this.name} says woof!`;
-//     }
+    bark(){
+        return `${this.name} says woof!`;
+    }
 
-//     sleep(){
-//         return `${this.name} is sleeping`;
-//     }
+    sleep(){
+        return `${this.name} is sleeping`;
+    }
+}
+
+
+class GuideDog extends Dog{
+    constructor(name, breed, owner){
+        super(name, breed);
+        this.owner = owner;
+    }
+
+    alert(){
+        return `${this.name} alerts you to danger, Good dog!`;
+    }
+}
+
+
+// function Dog(name, breed){
+//     console.log('This is:', this);
+//     this.name = name;
+//     this.breed = breed;
 // }
 
-function Dog(name, breed){
-    console.log('This is:', this);
-    this.name = name;
-    this.breed = breed;
-}
+// Dog.prototype.bark = function(){
+//     return `${this.name} says woof!`;
+// }
 
-Dog.prototype.bark = function(){
-    return `${this.name} says woof!`;
-}
-
-Dog.prototype.sleep = function(){
-    return `${this.name} is sleeping;`
-}
+// Dog.prototype.sleep = function(){
+//     return `${this.name} is sleeping;`
+// }
 
 // function User(name, email){
 //     this.name = name;
---
+++ Resources/04_OOPUnderTheHood/learn/create.js
@@ -0,0 +1,8 @@
+const person = {
+    sing(){
+        return "LALALA";
+    },
+    isHuman: true,
+}
+
+const prnv = Object.create(person);
