25f23c5ed94e0e989aa815d4fbd75dc9ead3151c
Pranov Sarath <pranov03@gmail.com>
2024-01-12 00:06:33 +0000

    Missed commit

--- Resources/06_LatestJS/learn/logical_assignment.js
+++ Resources/06_LatestJS/learn/logical_assignment.js
@@ -5,6 +5,14 @@ const todo = {
 
 console.log(todo.priority ||= "MEDIUM");
 
-
 console.log(todo.priority || (todo.priority = "MEDIUM"));
 
+
+
+
+//Logical AND=
+
+let num = 99;
+
+num &&= 50;
+console.log(num);
