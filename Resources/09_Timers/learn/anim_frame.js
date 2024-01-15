e4259773191b48d7fd7bbe6481309735e3255b8a
Pranov Sarath <pranov03@gmail.com>
2024-01-15 00:24:56 +0000

    Completed Chapter 10 - Timers

--- Resources/09_Timers/learn/anim_frame.js
+++ Resources/09_Timers/learn/anim_frame.js
@@ -2,10 +2,25 @@ const boxInterval = document.getElementById("boxInterval");
 const boxAnimationFrame = document.getElementById("boxAnimationFrame");
 
 let intervalAngle = 0;
+let animationFrameAngle = 0;
 
 function animateWithInterval(){
     boxInterval.style.transform = "rotate(" + intervalAngle + "deg)";
     intervalAngle += 2;
 }
 
-setInterval(animateWithInterval, 16); //60 FPS (appoximately)
+setInterval(animateWithInterval, 7); //60 FPS (appoximately)
+
+
+let previousTime;
+//define function to work with requestAnimationFrame
+function animateWithAnimationFrame(currentTime){
+    console.log(currentTime - previousTime);
+    previousTime = currentTime;
+    boxAnimationFrame.style.transform = "rotate(" + animationFrameAngle + "deg)";
+    animationFrameAngle += 2;
+    requestAnimationFrame(animateWithAnimationFrame);
+}
+
+//call requestAnimationFrame
+requestAnimationFrame(animateWithAnimationFrame);
